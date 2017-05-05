9def33b3846685d951b11de420aef148f4f4b2e5
Chris Hubbard <chris@thehubbards.org>
2017-05-05 16:53:58 -0400

    Change format
    * Need at least 4 columns (separated by ~)
      * title~subtitle~passage~text
    * pick up subtitle from passage column in T line

--- export-bsv-script.rb
+++ export-bsv-script.rb
@@ -9,10 +9,13 @@ end
 
 ARGV << "-h" if ARGV.empty?
 
-$options = { :title => 'Title' }
+$options = { :title => 'Title', :passage => '' }
 OptionParser.new do |opts|
   opts.banner = 'Usage: export_bsv_script.rb [options] FILENAME'
   opts.on('-t', '--title TITLE', 'Title of script') do |title|
+    $options[:title] = title
+  end
+  opts.on('-p', '--passage PASSAGE', 'Passage of script') do |title|
     $options[:title] = title
   end
   opts.on_tail("-h", "--help", "Show this message") do
@@ -31,6 +34,7 @@ filepath = ARGV[0]
 Dir.chdir(File.dirname(filepath))
 file = File.basename(filepath)
 title = $options[:title]
+passage = $options[:passage]
 
 doc = Docx::Document.open(file)
 table = doc.tables[0]
@@ -38,13 +42,14 @@ table.rows.each do |row|
   page = row.cells[0].nil? ? "" : row.cells[0].to_s
   if page == "T"
     title = row.cells[2].nil? ? title : row.cells[2].to_s
+    passage = row.cells[3].nil? ? passage : row.cells[3].to_s
   elsif page.is_integer?
     Dir.mkdir(title)  unless File.exists?(title)
     text = row.cells[2].nil? ? "" : row.cells[2].to_s
     ref = row.cells[3].nil? ? "" : row.cells[3].to_s
 
     File.open(File.join(title, page + ".txt"), "w+") do |f|
-      f.write("#{title.strip}~#{ref.strip}~#{text.strip}")
+      f.write("#{title.strip}~#{passage.strip}~#{ref.strip}~#{text.strip}")
     end
   end
 end
